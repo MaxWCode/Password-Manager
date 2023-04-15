2f6ec70b5b7de3e5a53a8b96b196af46da46b5cb
MaxWCode <maxwardle008@googlemail.com>
2023-04-15 22:04:35 +0100

    Build endpoints and base.html along with styling and sql models built

--- PasswordVault/PasswordVault/urls.py
+++ PasswordVault/PasswordVault/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('manager/', include("vault.urls"))
+    path('', include("vault.urls"))
 ]
+
