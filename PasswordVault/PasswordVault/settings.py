e94ed1cd03c53c651eaf369521c03fa199f5d44f
MaxWCode <maxwardle008@googlemail.com>
2023-05-27 03:13:01 +0100

    Added procfile, commit ready

--- PasswordVault/PasswordVault/settings.py
+++ PasswordVault/PasswordVault/settings.py
@@ -18,10 +18,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
+from django.core.management.utils import get_random_secret_key
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = get_random_secret_key()
 
 # Generate the encryption key and store it in an environment variable
 from dotenv import load_dotenv
