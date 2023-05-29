ca7ef9867e3f2b0252bc93827a4c40c9a5046af9
MaxWCode <maxwardle008@googlemail.com>
2023-05-29 16:38:33 +0100

    Added path command to build.sh, deploy ready

--- PasswordVault/build.sh
+++ PasswordVault/build.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
+
+echo "Running build.sh on Render..."
+
 # exit on error
 set -o errexit
 
+export PATH="/opt/render/.local/bin:$PATH"
 python manage.py collectstatic --no-input
 python manage.py migrate
 pip install gunicorn
