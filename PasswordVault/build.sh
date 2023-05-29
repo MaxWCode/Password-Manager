2e384ff89c97ec185375afe1b5b993f21589a9a2
MaxWCode <maxwardle008@googlemail.com>
2023-05-29 17:42:39 +0100

    Added build command, dp

--- PasswordVault/build.sh
+++ PasswordVault/build.sh
@@ -5,7 +5,8 @@ echo "Running build.sh on Render..."
 # exit on error
 set -o errexit
 
+pip install -r requirements.txt
 export PATH="/opt/render/.local/bin:$PATH"
 python manage.py collectstatic --no-input
 python manage.py migrate
-pip install gunicorn
+sudo apt install gunicorn3
