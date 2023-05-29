abc4b4d1eff4ae886cdbf3f61182484d549c6734
MaxWCode <maxwardle008@googlemail.com>
2023-05-29 17:47:50 +0100

    edited build command, dp

--- PasswordVault/build.sh
+++ PasswordVault/build.sh
@@ -6,8 +6,8 @@ echo "Running build.sh on Render..."
 set -o errexit
 
 pip3 install -r requirements.txt
-export PATH="/opt/render/.local/bin:$PATH"
 python manage.py collectstatic --no-input
 python manage.py migrate
 pip3 install git+https://github.com/benoitc/gunicorn.git
+export PATH="/opt/render/.local/bin:$PATH"
 
